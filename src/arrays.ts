aadfe4c1efa780dbf8e90fe1df7aecd03aeff519
aorlov05 <aorlov@udel.edu>
2025-02-23 15:09:26 -0500

    Find ends of book list

--- src/arrays.ts
+++ src/arrays.ts
@@ -5,7 +5,11 @@
  * the number twice.
  */
 export function bookEndList(numbers: number[]): number[] {
-    return numbers;
+    if (numbers.length < 1) {
+        return [];
+    }
+
+    return [numbers[0], numbers[numbers.length - 1]];
 }
 
 /**
@@ -13,8 +17,7 @@ export function bookEndList(numbers: number[]): number[] {
  * number has been tripled (multiplied by 3).
  */
 export function tripleNumbers(numbers: number[]): number[] {
-    return numbers;
-}
+    return [];
 
 /**
  * Consume an array of strings and convert them to integers. If
