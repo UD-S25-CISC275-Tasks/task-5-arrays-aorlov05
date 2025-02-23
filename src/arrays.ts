6398de2e91124185d5270d8cd29a9438314d5440
aorlov05 <aorlov@udel.edu>
2025-02-23 15:09:59 -0500

    Triple numbers in array

--- src/arrays.ts
+++ src/arrays.ts
@@ -17,7 +17,8 @@ export function bookEndList(numbers: number[]): number[] {
  * number has been tripled (multiplied by 3).
  */
 export function tripleNumbers(numbers: number[]): number[] {
-    return [];
+    return numbers.map((value: number): number => value * 3);
+}
 
 /**
  * Consume an array of strings and convert them to integers. If
